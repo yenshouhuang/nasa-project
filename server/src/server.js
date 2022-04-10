f84bc1b01b58f2cdf3361720fb0c10c248216d7c
Thomas Huang <yenshouhuang@gmail.com>
2022-04-09 22:23:34 -0500

    all functions

---
+++ server/src/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+
+
+
+require('dotenv').config();
+
+
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
+
+
+
+
+
+async function startServer() {
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
